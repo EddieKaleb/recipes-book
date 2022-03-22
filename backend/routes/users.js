9294f7c36eacd049f5882b6df6e12d58fd5823f5
Eddie Kaleb <eddie.fernandes@ccc.ufcg.edu.br>
2022-03-22 17:03:00 -0300

    Criacao controller de categorias, usuarios, receitas

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express"),
+  router = express.Router(),
+  userController = require("../controllers/users");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get("/", userController.index);
+router.get("/:id", userController.show);
 
 module.exports = router;

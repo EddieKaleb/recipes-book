1680ba96e4087ea48b01a7e28baa905ea54278ee
Eddie Kaleb <eddie.fernandes@ccc.ufcg.edu.br>
2022-03-27 20:39:00 -0300

    RecipeDetail - useReact adicionado

--- frontend/src/components/Recipes/RecipeDetail/RecipeDetail.js
+++ frontend/src/components/Recipes/RecipeDetail/RecipeDetail.js
@@ -1,7 +1,30 @@
+import { useState } from 'react'
 import { Title } from '../../Title/Title'
 import './RecipeDetail.css'
 
 export const RecipeDetail = () => {
+  const [recipe, setRecipe] = useState({
+    id: 1,
+    titulo: 'Cuscuz de Lilian',
+    tempo_preparo: 5,
+    rendimento: 10,
+    categoria: {
+      id: 1,
+      descricao: 'Bolos e Massas',
+    },
+    usuario: {
+      nome: 'Eddie',
+    },
+    ingredientes:
+      'Cuscuz (400g); Cimento (100g); Areia (50g); Água (1ml); Brita (50g)',
+    modo_preparo: 'Junte tudo e coloque no fogão',
+    observacoes: 'Não deixar molhado, espere ficar seco',
+    url_imagem:
+      'https://pbs.twimg.com/media/ExKoiIdWQAM5No1?format=jpg&name=large',
+    data: '2022-04-26 10:00:00',
+    url_video: 'https://www.youtube.com/embed/AYdFb6rD6K0',
+  })
+
   return (
     <div className="page">
       <Title title={"Recipe's Details"} />
@@ -9,18 +32,17 @@ export const RecipeDetail = () => {
         <div className="restaurant">
           <div className="container">
             <div className="content">
-              <a href="/restaurant-single/">
-                <img
-                  src="https://pbs.twimg.com/media/ExKoiIdWQAM5No1?format=jpg&name=large"
-                  alt=""
-                />
+              <a href="#">
+                <img src={recipe.url_imagem} />
                 <span className="mask"></span>
                 <div className="title">
-                  <h4>Cuscuz de Lilian</h4>
+                  <h4>{recipe.titulo}</h4>
                   <span className="subtitle">
-                    <i className="ti-timer"></i>4 min&nbsp;|&nbsp;
-                    <i className="ti-server"></i>10 servings&nbsp;|&nbsp;
-                    <i className="ti-book"></i>Bolos e Massas
+                    <i className="ti-timer"></i>
+                    {recipe.tempo_preparo} min&nbsp;|&nbsp;
+                    <i className="ti-server"></i> servings&nbsp;|&nbsp;
+                    <i className="ti-book"></i>
+                    {recipe.categoria.descricao}
                     <br />
                   </span>
                 </div>
@@ -41,7 +63,7 @@ export const RecipeDetail = () => {
               <div className="row content-text">
                 <iframe
                   height={400}
-                  src={'https://www.youtube.com/embed/AYdFb6rD6K0'}
+                  src={recipe.url_video}
                   frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                   allowFullScreen
@@ -59,10 +81,7 @@ export const RecipeDetail = () => {
               </div>
               <div className="row">
                 <div className="col">
-                  <div className="content-text">
-                    Cuscuz (400g); Cimento (100g); Areia (50g); Água (1ml);
-                    Brita (50g)
-                  </div>
+                  <div className="content-text">{recipe.ingredientes}</div>
                 </div>
               </div>
             </div>
@@ -76,9 +95,7 @@ export const RecipeDetail = () => {
               </div>
               <div className="row">
                 <div className="col">
-                  <div className="content-text">
-                    Junte tudo e coloque no fogão
-                  </div>
+                  <div className="content-text">{recipe.modo_preparo}</div>
                 </div>
               </div>
             </div>
@@ -92,9 +109,7 @@ export const RecipeDetail = () => {
               </div>
               <div className="row">
                 <div className="col">
-                  <div className="content-text">
-                    Não deixar molhado, espere ficar seco
-                  </div>
+                  <div className="content-text">{recipe.observacoes}</div>
                 </div>
               </div>
             </div>

1b7931f31c0365325b0889411d877af6cdf870b6
Eddie Kaleb <eddie.fernandes@ccc.ufcg.edu.br>
2022-03-27 21:43:47 -0300

    populando bd e atualizando scripts

--- backend/database/inserts_bd.sql
+++ backend/database/inserts_bd.sql
@@ -1,2 +1,31 @@
 use receitas;
-INSERT INTO receitas.categoria(descricao) values("Bolos e Tortas"), ("Carnes"), ("Aves"), ("Peixes Frutos do Mar"), ("Saladas e Molhos"), ("Sopas"), ("Massas"), ("Bebidas"), ("Doces e Sobremesas"), ("Lanches"), ("Alimentação Saudável");
+
+INSERT INTO receitas.categoria VALUES 
+(1,'Bolos e Tortas','http://iraja.com.br/wp-content/uploads/2017/09/banner-placeholder.jpg'),
+(2,'Carnes','http://iraja.com.br/wp-content/uploads/2017/09/banner-placeholder.jpg'),
+(3,'Aves','http://iraja.com.br/wp-content/uploads/2017/09/banner-placeholder.jpg'),
+(4,'Peixes Frutos do Mar','http://iraja.com.br/wp-content/uploads/2017/09/banner-placeholder.jpg'),
+(5,'Saladas e Molhos','http://iraja.com.br/wp-content/uploads/2017/09/banner-placeholder.jpg'),
+(6,'Sopas','http://iraja.com.br/wp-content/uploads/2017/09/banner-placeholder.jpg'),
+(7,'Massas','http://iraja.com.br/wp-content/uploads/2017/09/banner-placeholder.jpg'),
+(8,'Bebidas','http://iraja.com.br/wp-content/uploads/2017/09/banner-placeholder.jpg'),
+(9,'Doces e Sobremesas','http://iraja.com.br/wp-content/uploads/2017/09/banner-placeholder.jpg'),
+(10,'Lanches','http://iraja.com.br/wp-content/uploads/2017/09/banner-placeholder.jpg'),
+(11,'Alimentação Saudável','http://iraja.com.br/wp-content/uploads/2017/09/banner-placeholder.jpg');
+
+INSERT INTO receitas.usuario VALUES 
+(1,'glenioanderson@gmail.com','123456','glenio'),
+(2,'maria@gmail.com','1245','maria'),
+(3,'eddie@gmail.com','789','eddie'),
+(4,'lilian@gmail.com','34232','lilian'),
+(5,'hector@hotmail.com','4444','hector'),
+(6,'joselio@gmail.com','234','joselio'),
+(7,'patrick@gmail.com','234','patrick'),
+(8,'tarsila@gmail.com','234','tarsila');
+
+INSERT INTO receitas.receita VALUES 
+(1,'Bolinho de mandioca recheado',90,15,'1 quilo de mandioca; 2 gemas; 1 colher (sopa) de margarina; cheiro-verde a gosto picadinho bem pequeno; sal e pimenta-do-reino; Recheio: 200 g de carne moída; ','1- Cozinhe a mandioca até quase desmanchar, escorra e retire aquelas fibras grossas e amasse com a ajuda de um garfo (não use o processador, pois essa massa é bastante pesada); 2-Coloque a massa em uma tigela grande e espere esfriar um pouco. 3- Acrescente as 2 gemas, a margarina, cheiro verde e temperos, amasse bem e reserve. Para o recheio faça um refogado com a carne moída e deixe que fique bem sequinho, depois de frio acrescente a mussarela ralada. 4- Montagem: 5- Molhe as mãos na água e forme os bolinhos, coloque o recheio, feche e empane. 6- Frite em óleo quente. Pode ser congelado, antes de fritar.','Quer dar uma olhada em outra típica receita de bolinho brasileira? Confira a nossa receita de bolinho de chuva e aproveite essa delícia mais tarde!','https://i.ytimg.com/vi/8Q275rlYuGI/maxresdefault.jpg',3,10,'2022-03-27','https://www.youtube.com/watch?v=8Q275rlYuGI'),
+(2,'Café cremoso',15,5,'50 g de café solúvel; 2 xícaras de açúcar refinado; 1 xícara de água. ','1- Junte todos os ingredientes e bata na batedeira durante 15 minutos até ficar um creme; 2- Guarde no congelador em pote fechado; 3- Este creme é para ser servido do seguinte modo: 1 colher de sobremesa em 1 xícara de café quente (já pronto) ou 2 colheres de sobremesa em 1 xícara de leite fervendo; 4- Está pronta uma deliciosa bebida cremosa para aquecer as noites frias de inverno.','Acompanhe seu café cremoso com um bolinho. ','https://img.cybercook.com.br/receitas/814/cafe-cremoso-de-batedeira-1-840x480.jpeg?q=75',7,8,'2022-03-10','https://www.youtube.com/watch?v=YIqFmJ6QPAg'),
+(3,'Carne de panela de pressão',25,15,'1 kg de carne (acém, patinho ou lombo de porco); 8 cebolas grandes cortadas em rodelas; 3 ou 4 cubos de caldo de carne.','1- Forre o fundo de uma panela de pressão com metade das cebolas; 2- Esfarele um cubo de caldo de carne por cima das cebolas, adicione a carne, adicione outro cubo de caldo de carne e cubra com o restante das cebolas; 3- Feche a panela de pressão, sem adicionar água, e deixe cozinhar por 25 minutos; 4- Se preferir, adicione batatas em rodelas e deixe cozinhar por mais 10 minutos.','','https://vovopalmirinha.com.br/wp-content/uploads/2016/05/carne-de-panela-005.png',4,2,'2022-02-17','https://www.youtube.com/watch?v=4JAn2zSS05Y'),
+(4,'Pão de aveia de frigideira',2,1,'1 ovo; 1 fio de azeite; 1 pitada de sal (ou pode substituir por uma colher de mel caso queira doce); 2 colheres (sopa) de água. ','1- Misture os ingredientes em uma tigelinha; 2- A massa não pode ficar seca; 3- Caso fique, acrescente mais 1 colher de água; 4- Unte uma frigideira com um fio de azeite e frite o pão dos dois lados; 5- Deixe dourar bem.','Ótimo para pré e pós treinos. Tem poucas calorias e é uma delícia.','https://amorpelacomida.com.br/wp-content/uploads/2020/08/pao-de-aveia-na-frigideira-com-ovo.jpg',1,11,'2022-01-20','https://www.youtube.com/watch?v=wdN-dvxZFxc');
+
